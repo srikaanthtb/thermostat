edda6fb449e2f6c850bc463439c4927b9f9c4efd
srikaanthtb <srikaanth.t.b@gmail.com>
2020-03-19 15:26:34 +0000

    add css

--- interface.js
+++ interface.js
@@ -32,4 +32,19 @@ $(document).ready(function(){
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.usage);
   };
+  function displayWeather(city) {
+ var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+ var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+ var units = '&units=metric';
+ $.get(url + token + units, function(data) {
+   $('#current-temperature').text(data.main.temp);
+ });
+
+ displayWeather('London');
+
+ $('#select-city').submit(function(event) {
+   event.preventDefault();
+   var city = $('#current-city').val();
+   displayWeather(city);
+ });
 });
